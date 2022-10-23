0bca11dfab721ecf482911ba6ed32d27e0463c0b
unknown <alinasenkovich33@gmail.com>
2022-10-23 22:25:48 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import ContactForm from "./ContactForm/ContactForm";
+//import Conteiner from "./ContactForm/ContactForm";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  }
+
+  addContact = (task) => {
+    const searchSameName = this.state.contacts
+      .map((cont) => cont.name)
+      .includes(task.name);
+
+    if (searchSameName) {
+      alert(`${task.name} is already in contacts`);
+    } else if (task.name.length === 0) {
+      alert("Fields must be filled!");
+    } else {
+      const contact = {
+        ...task,
+        id: nanoid(),
+      };
+
+      this.setState((prevState) => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+    }
+  };
+
+  changeFilter = (filter) => {
+    this.setState({ filter });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter((contacts) =>
+      contacts.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  removeContact = (contactId) => {
+    this.setState((prevState) => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+
+        <ContactForm onAddContact={this.addContact} />
+        <h2>Contacts</h2>
+        {visibleContacts.length > 1 && (
+          <Filter value={filter} onChangeFilter={this.changeFilter} />
+        )}
+        {visibleContacts.length > 0 && (
+          <ContactList
+            contacts={visibleContacts}
+            onRemoveContact={this.removeContact}
+          />
+        )}
+      </div>
+    );
+  }
+}
